fbb12b22e88a52ea87f0b152658974534e8e6430
StasVZ <stimyl89@mail.ru>
2024-04-19 09:34:27 +0300

    marketplace

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+const product_name = 'Ford Mustang';
+const product_quantity = '5';
+const product_category = 'muscle car';
+const product_price = '71000';
+
+console.log (product_quantity*product_price);
